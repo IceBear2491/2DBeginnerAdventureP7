97951ad291362ef5590a41ce6ae1f22310fd54bf
IceBear2491 <salvadorparajon5@gmail.com>
2025-11-12 15:26:44 -0800

    Added environment and collisions
    I don't know what else to put, Lolang has collision

--- P7ParajonSalvador2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P7ParajonSalvador2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -5,19 +5,26 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
+
     public InputAction MoveAction;
 
     void Start()
     {
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
 
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+        move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 10.0f * Time.deltaTime;
-        transform.position = position;
+    }
+    void FixedUpdate() 
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * 10.0f * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
     }
 }
