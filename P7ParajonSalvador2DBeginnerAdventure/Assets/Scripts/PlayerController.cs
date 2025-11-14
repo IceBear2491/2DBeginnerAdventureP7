8d651d34b5b837c4969ab6f24e8f56e4ea1da125
IceBear2491 <salvadorparajon5@gmail.com>
2025-11-14 15:25:20 -0800

    Added Health and Damage Zones
    Super cool and awesome kiwi's versus the dastardly Damage Zones

--- P7ParajonSalvador2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P7ParajonSalvador2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -5,26 +5,59 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    // Variables for movement
+    public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
+    public float speed = 10.0f;
 
-    public InputAction MoveAction;
+    // Variables for Health
+    public int maxHealth = 5;
+    public int health { get {  return currentHealth; }}
+    int currentHealth;
+
+    // Variables for i-frames
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float damageCooldown;
 
     void Start()
     {
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth; 
     }
 
 
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);
+        if (isInvincible)
+        {
+            damageCooldown -= Time.deltaTime;
+            if (damageCooldown < 0)
+            {
+                isInvincible = false;
+            }
+        }
     }
     void FixedUpdate() 
     {
         Vector2 position = (Vector2)rigidbody2d.position + move * 10.0f * Time.deltaTime;
         rigidbody2d.MovePosition(position);
+    }
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0) 
+        {
+            if (isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            damageCooldown = timeInvincible;
+        }
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
